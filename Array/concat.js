16f08170350f4a7a35d3b3af1a8121889a85a9ef
prioarief <prioariefgunawangunawan@gmail.com>
2020-04-20 13:37:42 +0700

    start learn

---
+++ Array/concat.js
@@ -0,0 +1,8 @@
+// concat digunakan untuk menggabungkan 2 array atau lebih
+
+const arr1 = ["Prio", "Arief"];
+const arr2 = ["Gunawan"];
+const arr3 = ["Ganteng"];
+
+const result = arr1.concat(arr2, arr3);
+console.log(result);
